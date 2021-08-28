2797cba3d62ee2b14a79db15ae15b050925fab39
yusufaligokselsupplementler <37908301+yusufaligoksel@users.noreply.github.com>
2021-08-28 17:53:08 +0300

    category and products command - query classes created

---
+++ src/Automat/Automat.Application/Handlers/Product/Queries/GetProductQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automat.Application.Handlers.Product.Queries
+{
+    class GetProductQuery
+    {
+    }
+}
